e6dc7d09544a59f715d541b85b0741ae4b23b183
Klaud2699 <artem.v.maluga@gmail.com>
2019-11-24 21:20:54 +0300

    Complete docker-compose

--- chronicle-cpp-consumer/EosTokenFilter.cpp
+++ chronicle-cpp-consumer/EosTokenFilter.cpp
@@ -21,28 +21,28 @@ ft EosTokenFilter::filter_tbl_personality(const json& j_data)
 
 fpv EosTokenFilter::filter_tx_personality(const json& j_data)
 {
-    std::vector<std::string> notifies;
+    std::vector<std::string> transfers;
     json j_act_traces  = json::parse(j_data["trace"]["action_traces"].dump());
 
     for(auto& item : j_act_traces)
     {
-        if(item["act"]["name"] == "notify")
+        if(item["act"]["name"] == "transfer")
         {
             json j_not = json::parse(item["act"]["data"].dump());
             j_not["block_num"] = j_data["block_num"].get<std::string>();
             j_not["block_timestamp"] = j_data["block_timestamp"].get<std::string>();
             j_not["global_sequence"] = item["receipt"]["global_sequence"].get<std::string>();
             j_not["_id"] = item["receipt"]["global_sequence"].get<std::string>();
-            notifies.push_back(j_not.dump());
+            transfers.push_back(j_not.dump());
         }
     }
 
-    if (!notifies.empty())
+    if (!transfers.empty())
     {
-        return fpv("notifies",notifies);
+        return fpv("transfers",transfers);
     }
     else
     {
-        return fpv("",notifies);
+        return fpv("",transfers);
     }
 }
---
+++ eos-chronicle/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/eos-chronicle/build/chronicle-receiver --config-dir /build/eos-chronicle/config --data-dir /build/eos-chronicle/data 2>&1
